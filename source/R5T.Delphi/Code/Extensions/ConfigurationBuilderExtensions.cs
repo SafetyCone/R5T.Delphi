d79725272b32917c3a3aa19aaa8cfbe3276a1c72
MinexAutomation <minex.automation@gmail.com>
2020-03-03 23:36:03 -0800

    Follow through on ISecretsDirectoryFilePathProvider name change.

--- source/R5T.Delphi/Code/Extensions/ConfigurationBuilderExtensions.cs
+++ source/R5T.Delphi/Code/Extensions/ConfigurationBuilderExtensions.cs
@@ -12,7 +12,7 @@ namespace R5T.Delphi
     {
         public static IConfigurationBuilder AddDatabaseServerAuthentications(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider)
         {
-            var secretsFilePathProvider = configurationServiceProvider.GetRequiredService<ISecretsFilePathProvider>();
+            var secretsFilePathProvider = configurationServiceProvider.GetRequiredService<ISecretsDirectoryFilePathProvider>();
 
             var secretsFilePath = secretsFilePathProvider.GetSecretsFilePath(FileNames.DatabaseAuthenticationsJsonFileName);
 
